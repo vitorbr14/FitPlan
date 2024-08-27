88b55933c4b93428d6ff3cc7cb91fb34864eb7e4
Vitor Gomes <vitorgomes605@gmail.com>
2024-08-27 16:13:40 -0300

    Limpando

--- frontend/src/components/Alunos/NovoAluno/FormStep4.tsx
+++
@@ -1,8 +0,0 @@
-type TypeFormStep4 = {};
-export const FormStep4 = () => {
-  return (
-    <div className="col-span-12   ">
-      <h1 className="text-xl">O aluno VITOR foi cadastrado com sucesso</h1>
-    </div>
-  );
-};
